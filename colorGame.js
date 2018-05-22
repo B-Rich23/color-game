4a399640eaabf0c1d2ada3cc569cd8ce1b422716
Brandan Richardson <brandanr@gmail.com>
2018-05-22 00:12:54 -0700

    added logic to change background of h1 to correct color once clicked

--- colorGame.js
+++ colorGame.js
@@ -1,37 +1,23 @@
-// var colors = [
-//     "rgb(255, 0, 0)",
-//     "rgb(255, 255, 0)",
-//     "rgb(0, 255, 0)",
-//     "rgb(0, 255, 255)",
-//     "rgb(0, 0, 255)",
-//     "rgb(255, 0, 255)"
-// ];
+var colors = [
+   
+];
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = pickedColor();
 var displayColor = document.querySelector('#display');
 var message = document.getElementById('message');
 var h1 = document.querySelector('h1');
 var reset = document.querySelector('#new');
-var colors = [generateRandomColors(6)];
+var colors = generateRandomColors(6);
+var pickedColor;
 
-function squareRandom() {
-    for (var i = 0; squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-
-        squares[i].addEventListener('click', function () {
-            if (this.style.backgroundColor === pickedColor) {
-                message.textContent = "CORRECT!!!";
-                changeColors();
-            } else {
-                this.style.backgroundColor = '#232323';
-                message.textContent = "TRY AGAIN!!!";
-            }
-        });    
-    }   
+function pickedColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
 
+
 function pickedColorDisplay() {
+    pickedColor = pickedColor();
     displayColor.textContent = pickedColor;   
 }
 
@@ -45,17 +31,6 @@ function pickedColorDisplay() {
 //     }
 
 // }
-function pickedColor() {
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
-}
-
-function changeColors() {
-    for (var i = 0; squares.length; i++) {
-            squares[i].style.backgroundColor = pickedColor;
-            h1.style.backgroundColor = pickedColor;
-    }
-}
 
 function randomColor() {
     var r = Math.floor(Math.random() * 256);
@@ -79,8 +54,35 @@ function generateRandomColors(num) {
 
 }
 
+
+function changeColors() {
+    for (var i = 0; squares.length; i++) {
+        squares[i].style.backgroundColor = pickedColor;
+        h1.style.backgroundColor = pickedColor;
+    }
+}
+
+function squareRandom() {
+    for (var i = 0; squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        h1.style.backgroundColor = '#232323';
+
+        squares[i].addEventListener('click', function () {
+            if (this.style.backgroundColor === pickedColor) {
+                message.textContent = "CORRECT!!!";
+                changeColors();
+            } else {
+                this.style.backgroundColor = '#232323';
+                message.textContent = "TRY AGAIN!!!";
+            }
+        });
+    }
+}
+
+
 function newGame () {
     reset.addEventListener('click', function() {
+        pickedColorDisplay();
         squareRandom();
         // console.log('clicked');
     });
