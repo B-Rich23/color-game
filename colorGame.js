2eb2a3eaebd1c9672ac4da323fb5ae4939010046
Brandan Richardson <brandanr@gmail.com>
2018-05-21 23:56:21 -0700

    added logic to generate random rgb colors

--- colorGame.js
+++ colorGame.js
@@ -1,17 +1,19 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
+// var colors = [
+//     "rgb(255, 0, 0)",
+//     "rgb(255, 255, 0)",
+//     "rgb(0, 255, 0)",
+//     "rgb(0, 255, 255)",
+//     "rgb(0, 0, 255)",
+//     "rgb(255, 0, 255)"
+// ];
 
 var squares = document.querySelectorAll('.square');
-// var random = Math.floor(Math.random() * colors.length);
 var pickedColor = pickedColor();
 var displayColor = document.querySelector('#display');
 var message = document.getElementById('message');
+var h1 = document.querySelector('h1');
+var reset = document.querySelector('#new');
+var colors = [generateRandomColors(6)];
 
 function squareRandom() {
     for (var i = 0; squares.length; i++) {
@@ -51,9 +53,41 @@ function pickedColor() {
 function changeColors() {
     for (var i = 0; squares.length; i++) {
             squares[i].style.backgroundColor = pickedColor;
+            h1.style.backgroundColor = pickedColor;
     }
 }
 
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function generateRandomColors(num) {
+    // make array
+    var arr = [];
+
+    // add number of random colors to array
+    for (var i = 0; i < num; i++) {
+        // randomColor();
+        arr.push(randomColor());
+    }
+
+    // return array
+    return arr;
+
+}
+
+function newGame () {
+    reset.addEventListener('click', function() {
+        squareRandom();
+        // console.log('clicked');
+    });
+}
+
 pickedColorDisplay();
+newGame();
+
 // colorMatch();
 squareRandom();
